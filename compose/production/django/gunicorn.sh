dbc6d6bf71ccc7d7a2861ebc4f9d6f78b8a4d830
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-16 20:38:48 +0400

    commented celery .sh files

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -7,4 +7,3 @@ set -o nounset
 
 python /app/manage.py collectstatic --noinput
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
-# /usr/local/bin/daphne -b 0.0.0.0 -p 5000 config.asgi:channel_layer
