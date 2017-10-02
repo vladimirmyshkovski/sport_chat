9540e94ca3fc40a9d2295bfc5e2b8247d46738af
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-02 20:04:59 +0400

    added channels settings to production config

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -7,4 +7,4 @@ set -o nounset
 
 python /app/manage.py collectstatic --noinput
 #/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
-/usr/local/bin/daphne -b 0.0.0.0 -p 5000 config.asgi:channel_layer -v2
+/usr/local/bin/daphne -b 0.0.0.0 -p 5000 config.asgi:channel_layer
