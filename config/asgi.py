cbb4d0813fff8316a172a0cf08cb5748a3388578
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-02 14:56:10 +0400

    added channels settings to production config

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,7 @@
 import os
 from channels.asgi import get_channel_layer
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
-app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
-sys.path.append(os.path.join(app_path, 'sport_chat'))
-
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
 
 channel_layer = get_channel_layer()
 
